1430c30f72abd15be65c840c07c7681a250f93dc
Sinfroy <154357215+Sinfroy@users.noreply.github.com>
2024-01-06 16:15:25 +0300

    creating conflict

--- src/main/java/ru/sinfroy/Main.java
+++ src/main/java/ru/sinfroy/Main.java
@@ -2,7 +2,7 @@ package ru.sinfroy;
 
 public class Main {
 
-    private static String msg = "Hello qa.guru!";
+    private static String msg = "Hello qa.guru! with conflict";
     public static void main(String[] args) {
         System.out.println(msg);
     }
